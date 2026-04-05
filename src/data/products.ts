5c87b95e58b78094036d446975c7fc043ce0e08a
Abraham Berrios <abraham.berrios24@hotmail.com>
2026-04-05 18:45:42 -0400

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,3 +1,38 @@
+import zingWhiteBlack1 from "@/assets/zing-white-black-1.png";
+import zingWhiteBlack2 from "@/assets/zing-white-black-2.png";
+import zingWhiteBlack3 from "@/assets/zing-white-black-3.png";
+
+import zingAllWhite1 from "@/assets/zing-all-white-1.jpg";
+import zingAllWhite2 from "@/assets/zing-all-white-2.jpg";
+import zingAllWhite3 from "@/assets/zing-all-white-3.jpg";
+
+import enigmaIvory1 from "@/assets/enigma-ivory-1.jpg";
+import enigmaIvory2 from "@/assets/enigma-ivory-2.jpg";
+import enigmaIvory3 from "@/assets/enigma-ivory-3.jpg";
+
+import waveWhiteDarkGreen1 from "@/assets/wave-white-dark-green-1.jpg";
+import waveWhiteDarkGreen2 from "@/assets/wave-white-dark-green-2.jpg";
+import waveWhiteDarkGreen3 from "@/assets/wave-white-dark-green-3.jpg";
+
+import reboundAllWhite1 from "@/assets/rebound-all-white-1.jpg";
+import reboundAllWhite2 from "@/assets/rebound-all-white-2.jpg";
+import reboundAllWhite3 from "@/assets/rebound-all-white-3.jpg";
+
+import graceBeige1 from "@/assets/grace-beige-1.png";
+import graceBeige2 from "@/assets/grace-beige-2.png";
+import graceBeige3 from "@/assets/grace-beige-3.png";
+
+export interface Product {
+  id: string;
+  name: string;
+  subtitle: string;
+  price: number;
+  currency: string;
+  images: string[];
+  tag?: string;
+  features: string[];
+}
+
 export const products: Product[] = [
   {
     id: "barebarics-zing-white-black",
@@ -46,8 +81,6 @@ export const products: Product[] = [
     name: "Barebarics Wave",
     subtitle: "White & Dark Green · consultar disponibilidad",
     price: 0,
-subtitle: "White & Dark Green · consultar disponibilidad",
-tag: "Consultar",
     currency: "$",
     images: [
       waveWhiteDarkGreen1,
